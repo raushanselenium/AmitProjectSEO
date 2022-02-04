e86677a7ceb0ef94dd9b3d87debec8926a6492fd
Raushan Kumar <vicky.raushan@gmail.com>
2022-02-04 14:29:26 +0530

    Added premium page again with config file

--- src/test/java/pageObject/PremiumPage.java
+++ src/test/java/pageObject/PremiumPage.java
@@ -1,2 +1,27 @@
-package pageObject;public class PremiumPage {
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PremiumPage {
+
+    WebDriver driver;
+
+    public PremiumPage(WebDriver driver)
+    {
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(id = "close-button-container")
+    WebElement crossMark;
+
+    public void clickCrossMark()
+    {
+        crossMark.click();
+    }
+
+
 }
